df763fe2d666213f0f803aeb09010cb8e6c46e4a
Sugandha Duhan <sugandhaduhan@Sugandhas-MacBook-Pro.local>
2022-12-13 22:27:35 -0800

    Added Logger

--- CSS522_Project/src/StepDefinition/Target_E2E_Automation.java
+++ CSS522_Project/src/StepDefinition/Target_E2E_Automation.java
@@ -12,6 +12,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 
 import java.io.File;
 import java.time.Duration;
@@ -31,6 +34,7 @@ public class Target_E2E_Automation {
     static WebDriver driver;
     static Actions actions;
     static JavascriptExecutor jse;
+    static Logger logger;
     String systemProp = "webdriver.chrome.driver";
     String driverPath = "../chromedriver";
     static String targetUrl = "https://www.target.com/";
@@ -42,9 +46,14 @@ public class Target_E2E_Automation {
         System.setProperty(systemProp, driverPath);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
+
         driver = new ChromeDriver(options);
         actions = new Actions(driver);
         jse = (JavascriptExecutor)driver;
+
+        logger = Logger.getLogger(Target_E2E_Automation.class.getName());
+        PropertyConfigurator.configure("log4j.properties");
+
         driver.get(targetUrl);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -52,6 +61,7 @@ public class Target_E2E_Automation {
 
     @When("Select a category and subcategory")
     public void select_a_category_and_subcategory() throws InterruptedException {
+        logger.debug("Inside select a category and subcategory method");
         driver.findElement(By.linkText("Categories")).click();
         Thread.sleep(2000);
         driver.findElement(By.cssSelector("#overlay-5 > div:nth-child(1) > div > div > a:nth-child(4)>div")).click();
@@ -66,6 +76,7 @@ public class Target_E2E_Automation {
 
     @When("Apply filters")
     public void apply_filters() throws Exception {
+        logger.debug("Inside apply filters method");
         driver.findElement(By.xpath("//button[text()='Light Bulb Count']")).click();
         Thread.sleep(2000);
         WebElement element1 =  driver.findElement(By.id("chk-5zld6"));
@@ -79,6 +90,7 @@ public class Target_E2E_Automation {
 
     @When("Select product")
     public void select_product() throws Exception {
+        logger.debug("Inside select product method");
         driver.findElement(By.xpath("//*[@id=\"pageBodyContainer\"]/div[1]/div/div[9]/div/div[1]/div[2]/div/section/div/div[1]/div/div/div[1]/div[1]")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"pageBodyContainer\"]/div[1]/div[2]/div[2]/div/div[4]/div/div[1]/button[3]")).click();
@@ -97,6 +109,7 @@ public class Target_E2E_Automation {
 
     @Then("Add product to cart")
     public void add_product_to_cart() throws InterruptedException {
+        logger.debug("Inside add product to cart method");
         driver.findElement(By.id("addToCartButtonOrTextIdFor84273354")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//button[text()='Continue shopping']")).click();
@@ -105,12 +118,14 @@ public class Target_E2E_Automation {
 
     @Given("Go to home page")
     public void go_to_home_page() throws InterruptedException{
+        logger.debug("Inside go to home page method");
         driver.navigate().to("https://www.target.com/");
         Thread.sleep(2000);
     }
 
     @When("Enter the product to be searched {string}")
     public void enter_the_product_to_be_searched(String product) throws InterruptedException{
+        logger.debug("Inside enter the product to be searched method");
         driver.findElement(By.id("search")).clear();
         driver.findElement(By.id("search")).sendKeys(product);
         actions.sendKeys(Keys.ENTER).build().perform();
@@ -120,6 +135,7 @@ public class Target_E2E_Automation {
 
     @When("Apply filters to {string} {string} {string}")
     public void apply_filters_to(String product, String minPrice, String maxPrice) throws InterruptedException{
+        logger.debug("Inside apply filters to product method");
         if(product.equals("TV")){
             driver.findElement(By.xpath("//button[text()='Popular TV Screen Sizes']")).click();
             Thread.sleep(2000);
@@ -154,6 +170,7 @@ public class Target_E2E_Automation {
 
     @Then("Select and add to cart")
     public void select_and_add_to_cart() throws InterruptedException{
+        logger.debug("Inside select and add to cart method");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//button[text()='Add to cart']")).click();
         Thread.sleep(2000);
@@ -163,14 +180,16 @@ public class Target_E2E_Automation {
 
     @Given("Go to cart")
     public void go_to_cart() throws InterruptedException{
+        logger.debug("Inside go to cart method");
         driver.findElement(By.xpath("//a[text()='View cart & check out']")).click();
         this.product = "cart";
         Thread.sleep(3000);
-        logsMessage("Cart Loaded");
+        logger.info("Cart Loaded");
     }
 
     @Then("Take Screenshot")
     public void take_screenshot() throws Exception {
+        logger.debug("Inside take screenshot method");
         TakesScreenshot scrShot = ((TakesScreenshot) driver);
         File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
         LocalDateTime myDateObj = LocalDateTime.now();
@@ -181,6 +200,7 @@ public class Target_E2E_Automation {
     }
 
     void waitsForElement(String expectedElSelector, int expectedCount) { // explicit wait helper
+        logger.debug("Inside wait for elements method");
         WebDriverWait waitFor = new WebDriverWait(driver, Duration.ofSeconds(8));
         waitFor.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(expectedElSelector), expectedCount));
     }
@@ -194,67 +214,69 @@ public class Target_E2E_Automation {
         //return Integer.parseInt(price.replace(",","").substring(1,4));
     }
 
-    void logsMessage(String msg) {
-        System.out.println( "\t\t" + "\u2713" + "\t" + msg );
-    }
-
     @Given("Cart is loaded")
     public void checkIfCartLoaded () {
+        logger.debug("Inside cart loaded method");
         String listItemSelector = "div[data-test=\"cartItem\"]";
         waitsForElement(listItemSelector, 0);
-        logsMessage("Cart Loaded");
+        logger.info("Cart Loaded");
     }
 
     @Then("Verifies Cart Amount")
     public void verifiesCartTotalAmount () {
+        logger.debug("Inside cart total amount method");
         String listItemSelector = "div[data-test=\"cartItem\"]";
         waitsForElement(listItemSelector, 0);
         String totalAmtSelector = "div[data-test=\"cart-summary-total\"] p";
         String totalPrice = driver.findElement(By.cssSelector(totalAmtSelector)).getText();
         int priceAsInt = getPriceAsInt(totalPrice);
         Assert.assertTrue(priceAsInt >= 50);
-        logsMessage("Verified Total Amount");
+        logger.info("Verified Total Amount");
         addWait();
     }
 
     @Then("Verifies Delivery Fee")
     public void verifiesDeliveryInfo () {
+        logger.debug("Inside delivery info method");
         String deliveryFeeSelector = "div[data-test=\"cart-summary-delivery\"] div div:nth-child(2) p";
         String expectedDeliveryFee = "Free";
         String deliveryFeeDisplayed = driver.findElement(By.cssSelector(deliveryFeeSelector)).getText();
         Assert.assertTrue(deliveryFeeDisplayed.contains(expectedDeliveryFee));
-        logsMessage("Verified Delivery Fee");
+        logger.info("Verified Delivery Fee");
         addWait();
     }
 
     @Then("Verifies Checkout Button")
     public void verifiesCheckoutBtn () {
+        logger.debug("Inside checkout button method");
         String checkoutBtnSelector = "button[data-test=\"checkout-button\"]";
         WebElement checkoutBtn = driver.findElement(By.cssSelector(checkoutBtnSelector));
         Assert.assertTrue(checkoutBtn.getText().contains("check out"));
         Assert.assertTrue(checkoutBtn.isEnabled());
-        logsMessage("Verified Checkout Button");
+        logger.info("Verified Checkout Button");
         addWait();
     }
 
 
     @Then("Goes to checkout")
     public void verifiesSignInModalForNonAuth () {
+        logger.debug("Inside sign in modal for non auth method");
         String checkoutBtnSelector = "button[data-test=\"checkout-button\"]";
         WebElement checkoutBtn = driver.findElement(By.cssSelector(checkoutBtnSelector));
         checkoutBtn.click();
         String modalSelector = "div[data-test=\"@web/OverlayModal\"]";
         waitsForElement(modalSelector, 0);
-        logsMessage("Sign In Modal loaded for non-auth users");
+        logger.info("Sign In Modal loaded for non-auth users");
         addWait();
     }
 
     @Then("Validates Sign In Modal")
     public void validatesSignInForm () {
+        logger.debug("Inside validates sign in form method");
         WebElement title = driver.findElement(By.cssSelector("div[data-test=\"@web/OverlayModal\"] div[data-test=\"content-wrapper\"] h2"));
         String expectedSignInModalTitle = "Sign into your Target account";
         Assert.assertTrue(title.getText().contains(expectedSignInModalTitle));
-        logsMessage("Verified Title");
+        logger.info("Verified Title");
         String invalidTestUserName = "dfgfg@*(";
         String invalidTestPassword = "abc";
         driver.findElement(By.cssSelector("#username")).sendKeys(invalidTestUserName);
@@ -268,16 +290,17 @@ public class Target_E2E_Automation {
         WebElement usernameErrorMsg = driver.findElement(By.id("username--ErrorMessage"));
         Assert.assertTrue(usernameErrorMsg.isDisplayed());
         Assert.assertTrue(usernameErrorMsg.getText().equals("Please enter a valid email or phone number"));
-        logsMessage("Verified Username field validations");
+        logger.info("Verified Username field validations");
 
         WebElement passwordErrMsg = driver.findElement(By.id("password--ErrorMessage"));
         Assert.assertTrue(passwordErrMsg.isDisplayed());
         Assert.assertTrue(passwordErrMsg.getText().equals("Please enter a valid password"));
-        logsMessage("Verified Password field validations");
+        logger.info("Verified Password field validations");
     }
 
     @Then("Close and quit")
     public void close_and_quit(){
+        logger.debug("Closing all the sessions");
         driver.close();
         driver.quit();
     }
